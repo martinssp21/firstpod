7dc237b95b7124e1aa1fd8689bccf63993a01574
Rodrigo Martins <rodrigo.martins1@everis.com>
2019-04-05 16:14:58 -0300

    First upload POD

---
+++ AOModalStatus/AOModalStatus.h
@@ -0,0 +1,19 @@
+//
+//  AOModalStatus.h
+//  AOModalStatus
+//
+//  Created by Rodrigo Martins on 05/04/19.
+//  Copyright © 2019 Rodrigo Martins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AOModalStatus.
+FOUNDATION_EXPORT double AOModalStatusVersionNumber;
+
+//! Project version string for AOModalStatus.
+FOUNDATION_EXPORT const unsigned char AOModalStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AOModalStatus/PublicHeader.h>
+
+
